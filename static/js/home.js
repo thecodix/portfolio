f3f0bf638efe5dc6b72e301adfbe70a0a086eed8
Alfredo Romeu <romeu.alfred@gmail.com>
2021-05-18 23:27:46 +0200

    Changed intro

--- static/js/home.js
+++ static/js/home.js
@@ -2,12 +2,11 @@ $(function() {
   /* NOTE: hard-refresh the browser once you've updated this */
   $(".typed").typed({
     strings: [
-      "stat rish.human<br/>" + 
-      "><span class='caret'>$</span> skills: android, hardware-software interaction, camera, computer vision<br/> ^100" +
-      "><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +
-      "><span class='caret'>$</span> hobbies: football, travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
-      "><span class='caret'>$</span> alias: crearo <br/>" +
-      "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>"/*
+      "stat alfredo.romeu<br/>" +
+      "><span class='caret'>$</span> skills: python, backend development, databases, git<br/> ^100" +
+      "><span class='caret'>$</span> job: software developer at Curelator<br/> ^100" +
+      "><span class='caret'>$</span> hobbies: photography, sports, dancing, travelling<br/> ^300" +
+      "><span class='caret'>$</span> alias: thecodix <br/>" +
       "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
     ],
     showCursor: true,
